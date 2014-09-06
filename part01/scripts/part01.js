37e37f7b99bc69742c5d1ff9cb512a69b56dd249
SimulatedSimian <neomech@gmail.com>
2014-09-06 22:22:45 +0000

    building minimal webgl app

--- part01/scripts/part01.js
+++ part01/scripts/part01.js
@@ -1,5 +1,23 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-
+var webgl_part01 = {
+    canvas: undefined,
+    gl: undefined,
     
+    init: function() {
+        console.log("init");
+    	this.canvas = document.getElementById("webgl_part01");
+      	this.gl = this.canvas.getContext("webgl");
+        
+        this.renderFrame();
+    },
+    
+    renderFrame: function() {
+        
+        window.requestAnimationFrame (this.renderFrame.bind(webgl_part01));
+        this.gl.clearColor(1.0, 0.5, 0, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    webgl_part01.init();
 });
